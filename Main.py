f1810e7743655589ca4b51befb5664d992f9d6c5
Amresh2104 <60054130+Ranjan2104@users.noreply.github.com>
2021-06-11 18:21:34 +0530

    initial

---
+++ Main.py
@@ -0,0 +1,12 @@
+# Developed by Amresh Ranjan.
+
+import pywhatkit as kt
+
+#display welcome msg
+print("Let's gets Started!")
+
+source_path = 'Lion.jpg'
+target_path = 'demo_ascii_art.text'
+
+kt.image_to_ascii_art(source_path, target_path)
+
